b9c6f66ba755e6f60aef18f2fa055468e8f1347d
rrchai <73901500+rrchai@users.noreply.github.com>
2022-11-10 15:25:12 -0800

    update to extract filename

--- task2/macs2/run_model.sh
+++ task2/macs2/run_model.sh
@@ -7,7 +7,7 @@ mkdir -p $OUPUT_DIR
 
 # 1. run macs2 on each downsampled bam
 # 2. rename _summits.bed to .bed
-ls $INPUT_DIR/*.bam | parallel --max-procs=$NCORES --halt-on-error 2 \
+basename -a -s .bam $(ls $INPUT_DIR/*.bam) | parallel --max-procs=$NCORES --halt-on-error 2 \
   'macs2 callpeak --keep-dup all -t $INPUT_DIR/{}.bam --nomodel --shift 100 --extsize 200 -n {} --outdir $OUPUT_DIR &&
    mv $OUPUT_DIR/{}_summits.bed $OUPUT_DIR/{}.bed'
 
