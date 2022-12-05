8a1a872fdfc03e46dc16ab58d9341e23f6afc12c
rrchai <73901500+rrchai@users.noreply.github.com>
2022-12-05 15:44:56 -0500

    change to no compress required

--- task2/macs2/run_model.sh
+++ task2/macs2/run_model.sh
@@ -10,5 +10,3 @@ mkdir -p $OUPUT_DIR
 basename -a -s .bam $(ls $INPUT_DIR/*.bam) | parallel --max-procs=$NCORES --halt-on-error 2 \
   'macs2 callpeak --keep-dup all -t $INPUT_DIR/{}.bam --nomodel --shift 100 --extsize 200 -n {} --outdir $OUPUT_DIR &&
    mv $OUPUT_DIR/{}_summits.bed $OUPUT_DIR/{}.bed'
-
-tar -I pigz -cvf $OUPUT_DIR/predictions.tar.gz $OUPUT_DIR/*.bed
